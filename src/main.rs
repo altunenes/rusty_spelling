5706b4a90a58146e73bef2d82c220414b723e2ef
altunenes <enesaltun2@gmail.com>
2023-03-01 01:48:51 +0300

    minor update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 use std::io;
 use rand::prelude::SliceRandom;
 use tts::*;
-
-const ASCII_ART_1: &str = "\
- _____ _____ _____ _____ _____ _____ _____ _____ 
-|     |     |     |     |     |     |     |     |
-|  S  |  P  |  E  |  L  |  L  |  I  |  N  |  G  |
-|_____|_____|_____|_____|_____|_____|_____|_____|\n";
-
-const ASCII_ART_2: &str = "\
-Type 'exit' to end the program.\n";
+use colored::*;
 
 fn main() -> Result<(), Error> {
-    println!("{}", ASCII_ART_1);
-    println!("{}", ASCII_ART_2);
-    println!("Enter the words you want to practice, separated by commas:");
+    println!("{}", "Enter the words you want to practice, separated by commas. Type '/exit' to end the game:".green());
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
@@ -25,7 +15,7 @@ fn main() -> Result<(), Error> {
         .collect();
 
     if words.is_empty() {
-        println!("No words were entered. Please try again.");
+        println!("{}", "No words were entered. Please try again.".red());
         return Ok(());
     }
 
@@ -37,9 +27,9 @@ fn main() -> Result<(), Error> {
 
     'game_loop: while let Some(word) = words.choose(&mut rng) {
         tts.speak(word, true)?;
-        let hidden_word: String = word.chars().map(|_| '_').collect();
+        let hidden_word: String = word.chars().map(|_| "_ ".bold().to_string()).collect();
         println!("The word is: {}", hidden_word);
-        println!("Enter your guess:");
+        println!("{}", "Enter your guess:".bold());
 
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).unwrap();
@@ -49,20 +39,27 @@ fn main() -> Result<(), Error> {
         if guess == word {
             points += 1;
             correct_words.push(word.clone());
-            println!("Correct! You now have {} points.", points);
-        } else if guess == "exit" {
+            println!("{} {}", "Correct!".green().bold(), format!("You now have {} points.", points).green());
+        } else if guess == "/exit" {
+            println!("Thanks for playing! Here are your results:");
             break 'game_loop;
         } else {
             incorrect_guesses += 1;
-            println!("Incorrect. The word was {}.", word);
+            println!("{} {}", "Incorrect.".red().bold(), format!("The word was {}.", word).red());
         }
     }
 
-    println!("\nYou scored {} points!", points);
-    println!("\nYou made {} incorrect guesses.", incorrect_guesses);
-    println!("\nYou got the following words correct:");
-    for word in correct_words {
-        println!("{}", word);
+    println!("\n{}", format!("You scored {} points!", points).yellow().bold());
+    println!("\n{}", format!("You made {} incorrect guesses.", incorrect_guesses).yellow().bold());
+    if !correct_words.is_empty() {
+        println!("\n{}", "You got the following words correct:".yellow().bold());
+        let mut correct_counts = std::collections::HashMap::new();
+        for word in &correct_words {
+            *correct_counts.entry(word).or_insert(0) += 1;
+        }
+        for (word, count) in correct_counts.iter() {
+            println!("{} ({})", word.green().bold(), count);
+        }
     }
 
     Ok(())
