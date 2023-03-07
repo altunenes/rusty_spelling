9a8a4ff743ee50e32daca58027504b8b26868b85
altunenes <enesaltun2@gmail.com>
2023-03-07 18:55:11 +0300

    more efficient string interpolation

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod game;
 fn main() -> Result<(), Error> {
     route::print_intro();
     let result = game::play_game();
-    println!("\n{}", format!("You scored {} points!", result.score).yellow().bold());
+    let output= format!("You scored {} points!", result.score);
+    println!("{}", output.yellow().bold());
     let mut results = Results::new();
     results.add_result(result);
     results.save_results("results.json").unwrap();
